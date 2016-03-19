fbdf80db346b7e99a18d434aec45b82f40882e68
Andrew Burdyug <buran83@gmail.com>
2016-03-20 00:47:23 +0300

    Add: tests drafts

---
+++ tests/test_auth.py
@@ -0,0 +1,141 @@
+"""All test of auth should be described here."""
+
+import hashlib
+import unittest
+
+from bson.objectid import ObjectId
+from makechat.models import User
+from utils import prepare_request, make_request
+
+
+class TestMongo(unittest.TestCase):
+    """Test mongoengine models which described in mackechat.models."""
+
+    def setUp(self):
+        """Standart SetUp method of unittest.TestCase."""
+
+    def test_1_user_create(self):
+        """Attempt to create user."""
+        user = User.objects.create(
+            username='test', email='test@example.org', password='test')
+        self.assertEqual(user.username, 'test')
+        self.assertEqual(user.password, 'test')
+        self.assertEqual(user.email, 'test@example.org')
+        self.assertEqual(isinstance(user.id, ObjectId), True)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Standart tearDown method of unittest.TestCase."""
+        User.objects.delete()
+
+
+class TestLogin(unittest.TestCase):
+    """Test /api/login endpoint."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Standart SetUpClass method of unittest.TestCase."""
+        cls.api_url = 'http://makechat-web/api/login'
+        User.objects.create(
+            username='test', email='test@example.org',
+            password=hashlib.sha256('test'.encode('ascii')).hexdigest())
+
+    def test_1_valid_creds(self):
+        """Attempt to login with correct credentials."""
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test', 'password': 'test'}))
+        self.assertEqual(res.code, 200)
+
+    def test_2_invalid_creds(self):
+        """Attempt to login with incorrect credentials."""
+        # invalid password
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test', 'password': 'test1'}))
+        self.assertEqual(res.code, 401)
+
+        # invalid username
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test1', 'password': 'test'}))
+        self.assertEqual(res.code, 401)
+
+        # invalid username and password
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test1', 'password': 'test1'}))
+        self.assertEqual(res.code, 401)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Standart tearDownClass method of unittest.TestCase."""
+        User.objects.delete()
+
+
+class TestRegister(unittest.TestCase):
+    """Test /api/register endpoint."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Standart SetUpClass method of unittest.TestCase."""
+        cls.api_url = 'http://makechat-web/api/register'
+
+    def test_1_valid_form_data(self):
+        """Attempt to register with valid form data."""
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test2', 'email': 'test@example.com',
+                           'password1': 'test2', 'password2': 'test2'}))
+        self.assertEqual(res.code, 201)
+
+    def test_2_valid_form_data_username_exist(self):
+        """Attempt to register with valid form data and existing username."""
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test2', 'email': 'test@example.com',
+                           'password1': 'test2', 'password2': 'test2'}))
+        self.assertEqual(res.code, 400)
+        self.assertEqual(res.content.get('description'),
+                         'Username is already taken.')
+
+    def test_3_valid_form_data_email_exist(self):
+        """Attempt to register with valid form data and existing email."""
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test3', 'email': 'test@example.com',
+                           'password1': 'test3', 'password2': 'test3'}))
+        self.assertEqual(res.code, 400)
+        self.assertEqual(res.content.get('description'),
+                         'Email is already taken.')
+
+    def test_4_invalid_form_data(self):
+        """Attempt to register with invalid form data."""
+        # username, email, password2 are misssing
+        res = make_request(prepare_request(
+            self.api_url, {'password1': 'test2'}))
+        self.assertEqual(res.code, 400)
+        self.assertEqual(res.content.get('title'), 'Missing parameter')
+
+        # email, password2 are misssing
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test2', 'password1': 'test2'}))
+        self.assertEqual(res.code, 400)
+        self.assertEqual(res.content.get('title'), 'Missing parameter')
+
+        # password2 is misssing
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test2', 'email': 'test@example.com',
+                           'password1': 'test2'}))
+        self.assertEqual(res.code, 400)
+        self.assertEqual(res.content.get('title'), 'Missing parameter')
+
+        #  Passwords do not match
+        res = make_request(prepare_request(
+            self.api_url, {'username': 'test2', 'email': 'test@example.com',
+                           'password1': 'test2', 'password2': '2222'}))
+        self.assertEqual(res.code, 400)
+        self.assertEqual(res.content.get('description'),
+                         'Passwords do not match.')
+
+    @classmethod
+    def tearDownClass(cls):
+        """Standart tearDownClass method of unittest.TestCase."""
+        User.objects.delete()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/utils.py
@@ -0,0 +1,35 @@
+"""All tests utils should be described here."""
+
+import json
+import urllib.request
+import urllib.error
+
+
+def prepare_request(url, data, method='POST', is_json=True):
+    """Prepare urllib.request.Request object."""
+    headers = {}
+    if is_json:
+        headers.update({'Content-Type': 'application/json'})
+        data = json.dumps(data).encode('ascii')
+    else:
+        data = data.encode('ascii')
+    return urllib.request.Request(url, data, headers, method=method)
+
+
+def get_content(res):
+    """Decode JSON response data to dict."""
+    data = res.read().decode('ascii')
+    if data:
+        return json.loads(data)
+    return {}
+
+
+def make_request(req):
+    """Prepare prepared request."""
+    try:
+        res = urllib.request.urlopen(req)
+    except (urllib.error.URLError, urllib.error.HTTPError) as er:
+        res = er
+    finally:
+        res.content = get_content(res)
+        return res
